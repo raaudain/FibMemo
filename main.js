3aa4b52fcfe793bfb56be0a26ebe3b455c4e7d10
Ramon Audain <raudain123@gmail.com>
2020-07-13 00:27:12 +0000

    solution

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 function fibMemo(index, cache){
+  cache = cache || [];
 
+  if (index < 2) return 1;
+
+  if (cache[index]) return cache[index];
+
+  return cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache)
 }
 
 
+fibMemo(5);
